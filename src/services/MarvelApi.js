135f781fab5fea08da0266a876bc47226a8dc057
lianker lopes <lianker.dev@gmail.com>
2020-01-06 20:21:52 -0300

    Create geters to keys(private and public) to handle errors if undefined values
    Private and Public keys are a important data in requests to marvel api, this
    commit add methods get to handle with undefined values.

--- src/services/MarvelApi.js
+++ src/services/MarvelApi.js
@@ -1,10 +1,30 @@
+import axios from "axios"
+import CryptoJs from 'crypto-js'
+
 const BASE_URL = 'http://gateway.marvel.com'
+const API_VERSION = 'v1'
 
 export default class MarvelApi {
-    constructor(options){
-        this.publicKey = options.publicKey
-        this.privateKey = options.privateKey
-        this.apiUrl = options.apiUrl || BASE_URL
+  constructor(options) {
+    this._publicKey = options.publicKey
+    this._privateKey = options.privateKey
+    this.apiUrl = options.apiUrl || BASE_URL
+    this.apiVersion = options.apiVersion || API_VERSION
+  }
+
+  get privateKey(){
+    if (!this._privateKey) {
+        throw new Error('invalid private key')
     }
     
+    return this._privateKey
+  }
+  
+  get publicKey(){
+    if (!this._publicKey) {
+        throw 'invalid public key'
+    }  
+
+    return this._publicKey
+  }
 }
--- src/services/MarvelApi.test.js
+++ src/services/MarvelApi.test.js
@@ -12,23 +12,45 @@ describe('MarvelApi', () => {
     })
     expect(api.publicKey).toEqual('bla')
   })
-  
+
+  it('Should throw exception if invalid publilcKey', () => {
+    const api = new MarvelApi({ privateKey: 'bar' })
+    expect(() => api.publicKey).toThrow('invalid public key')
+  })
+
   it('Should receive privateKey as an option', () => {
     const api = new MarvelApi({
       privateKey: 'foo'
     })
     expect(api.privateKey).toEqual('foo')
   })
-  
+
+  it('Should throw exception if invalid privateKey', () => {
+    const api = new MarvelApi({ publicKey: 'bar' })
+    expect(() => api.privateKey).toThrow('invalid private key')
+  })
+
   it('Should receive apiUrl as an option', () => {
     const api = new MarvelApi({
       apiUrl: 'http://myapi.com'
     })
     expect(api.apiUrl).toEqual('http://myapi.com')
   })
-  
+
   it('Should use default apiUrl if not provided', () => {
     const api = new MarvelApi({})
     expect(api.apiUrl).toEqual('http://gateway.marvel.com')
+  })
+
+  it('Should receive apiVersion as an option', () => {
+    const api = new MarvelApi({
+      apiVersion: 'v3'
+    })
+    expect(api.apiVersion).toEqual('v3')
+  })
+
+  it('Should use default apiVersion if not provided', () => {
+    const api = new MarvelApi({})
+    expect(api.apiVersion).toEqual('v1')
   })
 })
