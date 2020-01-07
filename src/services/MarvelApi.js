847157e5945fb48d926d0ff468709b8f8a22033a
lianker lopes <lianker.dev@gmail.com>
2020-01-07 07:50:58 -0300

    Add .getComics() method in MarvelApi
    This method gets all comics based on a filter that indicates offset, limit
    and orderBy clause.

--- src/services/MarvelApi.js
+++ src/services/MarvelApi.js
@@ -1,4 +1,4 @@
-import axios from "axios"
+import axios from 'axios'
 import CryptoJs from 'crypto-js'
 
 const BASE_URL = 'http://gateway.marvel.com'
@@ -12,19 +12,55 @@ export default class MarvelApi {
     this.apiVersion = options.apiVersion || API_VERSION
   }
 
-  get privateKey(){
+  get privateKey() {
     if (!this._privateKey) {
-        throw new Error('invalid private key')
+      throw new Error('invalid private key')
     }
-    
+
     return this._privateKey
   }
-  
-  get publicKey(){
+
+  get publicKey() {
     if (!this._publicKey) {
-        throw 'invalid public key'
-    }  
+      throw new Error('invalid public key') 
+    }
 
     return this._publicKey
+  }
+
+  generateHash(timestamp, privateKey, publicKey) {
+    return CryptoJs.MD5(`${timestamp}${privateKey}${publicKey}`).toString()
+  }
+
+  createDefaultParams(privateKey, publicKey, timestamp = Date.now()) {
+    return {
+      ts: timestamp,
+      apikey: publicKey,
+      hash: this.generateHash(timestamp, privateKey, publicKey)
+    }
+  }
+
+  async getComics(filters = {}) {
+    const { offset, limit, orderBy } = filters
+
+    const defaultParams = this.createDefaultParams(
+      this.privateKey,
+      this.publicKey
+    )
+
+    const options = {
+      params: {
+        ...defaultParams,
+        offset: offset || 0,
+        limit: limit || 20,
+        orderBy: orderBy || '-onsaleDate'
+      }
+    }
+
+    const comicsUrl = `${BASE_URL}/${API_VERSION}/public/comics`
+
+    return axios.get(comicsUrl, options).then(response => {
+      return response.data.data.results
+    })
   }
 }
--- src/services/MarvelApi.test.js
+++ src/services/MarvelApi.test.js
@@ -1,56 +1,148 @@
+import axios from 'axios'
+
 import MarvelApi from './MarvelApi'
 
+jest.mock('axios')
+
 describe('MarvelApi', () => {
-  it('Should create a instance of Marvel api', () => {
-    const api = new MarvelApi({})
-    expect(api).toBeInstanceOf(MarvelApi)
-  })
-
-  it('Should receive publilcKey as an option', () => {
-    const api = new MarvelApi({
-      publicKey: 'bla'
+  describe('new instance creation', () => {
+    it('Should create a instance of Marvel api', () => {
+      const api = new MarvelApi({})
+      expect(api).toBeInstanceOf(MarvelApi)
     })
-    expect(api.publicKey).toEqual('bla')
-  })
 
-  it('Should throw exception if invalid publilcKey', () => {
-    const api = new MarvelApi({ privateKey: 'bar' })
-    expect(() => api.publicKey).toThrow('invalid public key')
-  })
-
-  it('Should receive privateKey as an option', () => {
-    const api = new MarvelApi({
-      privateKey: 'foo'
+    it('Should receive publilcKey as an option', () => {
+      const api = new MarvelApi({
+        publicKey: 'bla'
+      })
+      expect(api.publicKey).toEqual('bla')
     })
-    expect(api.privateKey).toEqual('foo')
-  })
 
-  it('Should throw exception if invalid privateKey', () => {
-    const api = new MarvelApi({ publicKey: 'bar' })
-    expect(() => api.privateKey).toThrow('invalid private key')
-  })
-
-  it('Should receive apiUrl as an option', () => {
-    const api = new MarvelApi({
-      apiUrl: 'http://myapi.com'
+    it('Should throw exception if invalid publilcKey', () => {
+      const api = new MarvelApi({ privateKey: 'bar' })
+      expect(() => api.publicKey).toThrow('invalid public key')
     })
-    expect(api.apiUrl).toEqual('http://myapi.com')
-  })
 
-  it('Should use default apiUrl if not provided', () => {
-    const api = new MarvelApi({})
-    expect(api.apiUrl).toEqual('http://gateway.marvel.com')
-  })
-
-  it('Should receive apiVersion as an option', () => {
-    const api = new MarvelApi({
-      apiVersion: 'v3'
+    it('Should receive privateKey as an option', () => {
+      const api = new MarvelApi({
+        privateKey: 'foo'
+      })
+      expect(api.privateKey).toEqual('foo')
     })
-    expect(api.apiVersion).toEqual('v3')
+
+    it('Should throw exception if invalid privateKey', () => {
+      const api = new MarvelApi({ publicKey: 'bar' })
+      expect(() => api.privateKey).toThrow('invalid private key')
+    })
+
+    it('Should receive apiUrl as an option', () => {
+      const api = new MarvelApi({
+        apiUrl: 'http://myapi.com'
+      })
+      expect(api.apiUrl).toEqual('http://myapi.com')
+    })
+
+    it('Should use default apiUrl if not provided', () => {
+      const api = new MarvelApi({})
+      expect(api.apiUrl).toEqual('http://gateway.marvel.com')
+    })
+
+    it('Should receive apiVersion as an option', () => {
+      const api = new MarvelApi({
+        apiVersion: 'v3'
+      })
+      expect(api.apiVersion).toEqual('v3')
+    })
+
+    it('Should use default apiVersion if not provided', () => {
+      const api = new MarvelApi({})
+      expect(api.apiVersion).toEqual('v1')
+    })
   })
 
-  it('Should use default apiVersion if not provided', () => {
-    const api = new MarvelApi({})
-    expect(api.apiVersion).toEqual('v1')
+  describe('.getComics()', () => {
+    let api
+    let defaultParams
+
+    beforeEach(() => {
+      axios.get.mockResolvedValue({
+        data: {
+          data: {
+            results: [{comic: {id: 1}}]
+          }
+        }
+      })
+
+      defaultParams = {
+        ts: 23,
+        apikey: 'bar',
+        hash: 'c6703fab7ee263e9181e3735bee11525'
+      }
+
+      api = new MarvelApi({
+        publicKey: 'bar',
+        privateKey: 'foo'
+      })
+
+      jest.spyOn(api, 'createDefaultParams').mockReturnValue(defaultParams)
+    })
+
+    it('should call axios.get method', async () => {
+      await api.getComics({})
+
+      expect(axios.get).toHaveBeenCalled()
+    })
+
+    it('should add default filter values to axios params', async () => {
+      // Arrange
+      const options = {
+        params: {
+          ...defaultParams,
+          limit: 20,
+          offset: 0,
+          orderBy: '-onsaleDate'
+        }
+      }
+
+      // Act
+      await api.getComics({})
+
+      // Assert
+      expect(axios.get).toBeCalledWith(
+        'http://gateway.marvel.com/v1/public/comics',
+        options
+      )
+    })
+
+    it('should use filters from args to axios params', async () => {
+      // Arrange
+      const options = {
+        params: {
+          ...defaultParams,
+          limit: 10,
+          offset: 10,
+          orderBy: 'title'
+        }
+      }
+
+      // Act
+      await api.getComics({
+        limit: 10,
+        offset: 10,
+        orderBy: 'title'
+      })
+
+      // Assert
+      expect(axios.get).toBeCalledWith(
+        'http://gateway.marvel.com/v1/public/comics',
+        options
+      )
+    })
+
+    it('Should return correct response', async () => {
+      const result = await api.getComics({})
+
+      expect(result).toEqual([{comic: {id: 1}}])
+    })
   })
 })
